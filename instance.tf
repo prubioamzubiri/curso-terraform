e5745c7ea62d55f2fc71cf284a4e0cb98e4d0c32
=prubioam <=prubioam@zubirimanteo.com>
2023-07-03 08:38:22 +0200

    a

--- instance.tf
+++ instance.tf
@@ -1,24 +1,13 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+resource "aws_instance" "mi-primera-instancia" {
+  count         = "5"
+  ami           = "ami-04cbc90abb08f0321"
   instance_type = "t2.micro"
 
+  security_groups = ["default"]
+
   tags = {
-    Name = "HelloWorld"
+    name = "instancia-${count.index}"
   }
+
 }
+
