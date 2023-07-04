ea1f981809db78c0e1ec46209764eb62f191d2ec
=prubioam <=prubioam@zubirimanteo.com>
2023-07-04 13:01:36 +0200

    definir route table

--- vpc.tf
+++ vpc.tf
@@ -30,3 +30,12 @@ resource "aws_internet_gateway" "gateway-1" {
     }
   
 }
+
+resource "aws_route_table" "tabla-de-enrutamiento"{
+    vpc_id = aws_vpc.primera-vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gateway-1.id
+    }
+}
